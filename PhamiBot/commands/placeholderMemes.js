03f05e85750de38119ee86fd07688c77de6640ec
Phamishan Packiaseelan <phamishan93@gmail.com>
2026-01-29 19:04:25 +0100

    fix :pray:

--- PhamiBot/commands/placeholderMemes.js
+++ PhamiBot/commands/placeholderMemes.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, InteractionContextType } = require("discord.js");
 
-const filePath = "../pics/placeholderMemes/";
+const filePath = "../PhamiBot/pics/placeholderMemes/";
 
 var pics = [
     { files: [filePath + "gameExpoPhamiAndPatrick.png"] },
